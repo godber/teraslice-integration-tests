1a5f453e9e6f8de741b89cd456f2c6049552444e
Jared Noble <jsnoble1@gmail.com>
2016-11-03 15:18:58 -0700

    added new indicies and tests for id_slicer

--- spec/integration/data/id_slicer.js
+++ spec/integration/data/id_slicer.js
@@ -13,7 +13,6 @@ module.exports = function() {
 
             teraslice.jobs.submit(job_spec)
                 .then(function(job) {
-                    console.log('whats the job');
                     expect(job).toBeDefined();
                     expect(job.id()).toBeDefined();
 
@@ -33,7 +32,59 @@ module.exports = function() {
 
     });
 
-    it('job should complete after lifecycle changes', function(done) {
+    it('should reindex data by hexadecimal ids', function(done) {
+        var job_spec = _.cloneDeep(require('../../fixtures/jobs/id.json'));
+        job_spec.operations[0].key_type = 'hexadecimal';
+        job_spec.operations[0].index = 'example-hexadecimal-logs';
+        job_spec.operations[1].index = "test-hexadecimal-logs";
+
+        teraslice.jobs.submit(job_spec)
+            .then(function(job) {
+                expect(job).toBeDefined();
+                expect(job.id()).toBeDefined();
+
+                // The job may run for a while so we have to wait for it to finish.
+                return job.waitForStatus('completed');
+            })
+            .then(function() {
+                return es_helper.documentCountForIndex('test-hexadecimal-logs')
+                    .then(function(stats) {
+                        expect(stats.count).toBe(10000);
+                        expect(stats.deleted).toBe(0);
+                    });
+            })
+            .catch(fail)
+            .finally(done)
+    });
+
+    it('will only search on key_range', function(done) {
+        var job_spec = _.cloneDeep(require('../../fixtures/jobs/id.json'));
+        job_spec.operations[0].key_type = 'hexadecimal';
+        job_spec.operations[0].key_range = ['a', 'b', 'c', 'd', 'e'];
+
+        job_spec.operations[0].index = 'example-hexadecimal-logs';
+        job_spec.operations[1].index = 'test-keyrange-logs';
+
+        teraslice.jobs.submit(job_spec)
+            .then(function(job) {
+                expect(job).toBeDefined();
+                expect(job.id()).toBeDefined();
+
+                // The job may run for a while so we have to wait for it to finish.
+                return job.waitForStatus('completed');
+            })
+            .then(function() {
+                return es_helper.documentCountForIndex('test-keyrange-logs')
+                    .then(function(stats) {
+                        expect(stats.count).toBe(5000);
+                        expect(stats.deleted).toBe(0);
+                    });
+            })
+            .catch(fail)
+            .finally(done)
+    });
+
+    it('id_reader should complete after stopping and restarting', function(done) {
         var job_spec = _.cloneDeep(require('../../fixtures/jobs/id.json'));
         // Job needs to be able to run long enough to cycle
         job_spec.operations[1].index = "test-id_reindex-lifecycle-10000";
