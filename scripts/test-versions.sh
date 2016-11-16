1afdfeae975e0e257ada9fbba4695dab109f80e9
Austin Godber <godber@uberhip.com>
2016-11-16 09:07:04 -0700

    Added master.log

--- scripts/test-versions.sh
+++ scripts/test-versions.sh
@@ -10,13 +10,13 @@ function cleanup() {
 
 trap cleanup SIGINT
 
-echo "Test run began at: $(date)"
+echo "Test run began at: $(date)" | tee "logs/master.log"
 
 for version in "${versions[@]}"
 do
     echo ""
     echo "##############################################"
-    echo "  Running tests for Elasticsearch ${version}  "
+    echo "  Running tests for Elasticsearch ${version}  " | tee -a "logs/master.log"
     echo "##############################################"
     echo ""
 
@@ -24,12 +24,12 @@ do
     mkdir -p ${dir}
 
     npm config set teraslice-integration-tests:esVersion ${version}
-    echo "  Running setup for version ${version}."
+    echo "  Running setup for version ${version}." | tee -a "logs/master.log"
     npm run setup | tee ${dir}/setup.log 2>&1
     sleep 5
 
-    echo "  Running tests for version ${version}."
-    npm test | tee ${dir}/test.log 2>&1
+    echo "  Running tests for version ${version}." | tee -a "logs/master.log"
+    npm test | tee ${dir}/test.log 2>&1 | tee -a "logs/master.log"
     sleep 5
 
     docker-compose -f spec/docker-compose-es${version}.yml stop > ${dir}/docker-stop.log 2>&1
@@ -37,12 +37,12 @@ do
     docker-compose -f spec/docker-compose-es${version}.yml logs > ${dir}/docker-compose.log 2>&1
     sleep 2
 
-    echo "  Cleaning up for version ${version} at $(date)"
+    echo "  Cleaning up for version ${version} at $(date)" | tee -a "logs/master.log"
     docker rm $(docker ps -qa --filter 'name=spec_.*') > ${dir}/cleanup.log 2>&1
     docker volume rm spec_testdata >> ${dir}/cleanup.log 2>&1
 done
 
-echo "Test run completed at: $(date)"
+echo "Test run completed at: $(date)" | tee -a "logs/master.log"
 echo ""
 
 find logs
